86077619c738db703fdca2ea75477f3f84e4ccc9
Daniel Beeke <daniel@studiofonkel.nl>
2015-12-18 18:54:15 +0100

    lat lng

--- app/js/members.js
+++ app/js/members.js
@@ -1,9 +1,17 @@
-var map = L.map('leaflet-map').setView([51.505, -0.09], 13);
+$(function() {
 
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+    var map = L.map('leaflet-map').setView([51.505, -0.09], 13);
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([51.5, -0.09]).addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+
+    new L.Control.GeoSearch({
+        provider: new L.GeoSearch.Provider.Google()
+    }).addTo(map);
+
+});
