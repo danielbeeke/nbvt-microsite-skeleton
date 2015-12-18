d8e69bdc303d2732ad2b837cffe483740455bc60
Daniel Beeke <daniel@studiofonkel.nl>
2015-12-18 23:42:58 +0100

    members are shown on a map and only the first four are show

--- app/js/members.js
+++ app/js/members.js
@@ -1,17 +1,50 @@
 $(function() {
+    var map = L.map('leaflet-map');
+    var markerLayer = L.featureGroup().addTo(map);
 
-    var map = L.map('leaflet-map').setView([51.505, -0.09], 13);
+    L.tileLayer(window.nbvt.members.map).addTo(map);
 
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    var bounds = [];
 
-    L.marker([51.5, -0.09]).addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+    $.each(window.nbvt.members.items, function (delta, member) {
+        bounds.push([member.lat, member.lng]);
+    });
+
+    map.fitBounds(bounds);
 
     new L.Control.GeoSearch({
-        provider: new L.GeoSearch.Provider.Google()
+        provider: new L.GeoSearch.Provider.Google(),
+        showMarker: false
     }).addTo(map);
 
+    map.on('geosearch_foundlocations', function (locations) {
+        setSearch(locations.Locations[0].Y, locations.Locations[0].X)
+    });
+
+    function setSearch(lat, lng) {
+        markerLayer.clearLayers();
+
+        var membersToSort = [];
+
+        $.each(window.nbvt.members.items, function (delta, member) {
+            member.diff = Math.abs(member.lat - lat) + Math.abs(member.lng - lng);
+            membersToSort.push(member)
+        });
+
+        var membersSorted = membersToSort.sort(function (a, b){
+            return a.diff - b.diff;
+        });
+
+        var membersToShow = membersSorted.slice(0, 4);
+
+        var bounds = [];
+
+        $.each(membersToShow, function (delta, member) {
+            bounds.push([member.lat, member.lng]);
+
+            L.marker([member.lat, member.lng]).addTo(markerLayer).bindPopup(member.name)
+        });
+
+        map.fitBounds(bounds);
+    }
 });
