6eb9d9f56645c57e4dad7f664eaa600b289b9bce
Daniel Beeke <daniel@studiofonkel.nl>
2015-12-20 21:05:52 +0100

    component slider javascript done

--- app/js/slides.js
+++ app/js/slides.js
@@ -2,12 +2,29 @@ $(function() {
 
 
     $('#slides').owlCarousel({
-        navigation : true,
         slideSpeed : 300,
         paginationSpeed : 400,
-        singleItem:true
+        singleItem: true
     });
 
+
+    $('.slide-item-details').owlCarousel({
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem: true,
+        afterAction: function () {
+            var tabs = this.$elem.parent().find('.details-tabs');
+            var currentTab = tabs.find('.details-tab:nth-child(' + (this.currentItem + 1) + ')');
+            tabs.find('.details-tab').removeClass('active');
+            currentTab.addClass('active');
+        }
+    });
+
+
+    $('.details-tab').on('click', function () {
+        var parentSliderId = $(this).parent().data('for')
+        $('#' + parentSliderId).data('owlCarousel').goTo($(this).index());
+    })
 
 });
 
