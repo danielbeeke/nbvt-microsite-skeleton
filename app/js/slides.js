f89a271681a6479fdc0dbc4b6ff5c888cbf83b05
Daniel Beeke <daniel@studiofonkel.nl>
2015-12-20 20:16:13 +0100

    Adding owl slider

---
+++ app/js/slides.js
@@ -0,0 +1,13 @@
+$(function() {
+
+
+    $('#slides').owlCarousel({
+        navigation : true,
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true
+    });
+
+
+});
+
