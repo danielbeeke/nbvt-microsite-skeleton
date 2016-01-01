d96444ce19b95a1d6cd8d11486286703ef75e770
Daniel Beeke <daniel@studiofonkel.nl>
2016-01-01 16:59:58 +0100

    adding global disqus

---
+++ app/js/components/blogs/disqus.js
@@ -0,0 +1,16 @@
+(function($) {
+    /* * * CONFIGURATION VARIABLES * * */
+    var disqus_shortname = 'nbvt';
+
+    var disqus_config = function () {
+        this.page.url = 'nbvt.nl';
+        this.page.identifier = 'node/2736';
+    };
+
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function() {
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
+}(jQuery));
